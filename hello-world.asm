0f0998d8c69b35ee2131f5e4b84f8419b291f866
Herald Carl Avila <144099137+Devcavi19@users.noreply.github.com>
2024-09-05 15:49:52 +0800

    Update hello-world.asm

--- hello-world.asm
+++ hello-world.asm
@@ -1,6 +1,6 @@
 global  _main ; Declare the _main symbol as global
 extern  _printf ; Declare the _printf function
-section .text
+section .text ; Specify the code in the .text section
 _main: push message ; Push the address of the message
        call _printf ; _printf functions called to print msg.
        add esp, 4 ; adjust stack pointer by 4 bytes
