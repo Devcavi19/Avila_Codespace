1d97cd2bdc70083594e27b0dbd7b436c3d0ddfb7
Herald Carl Avila <144099137+Devcavi19@users.noreply.github.com>
2024-09-25 00:31:13 +0000

    iasAct2updt

--- ASM/lt3.asm
+++ ASM/lt3.asm
@@ -5,24 +5,24 @@
 ; output name, age, address
 ; Nice to meet you, userName, userAge, who lives at userAddress
 
-section .data
+section .data ; section .data is used for declaring initialized data or constants. In our case, we used prompt1, prompt2, prompt3, input_name, input_age, input_address, and output_msg.
     prompt1 db 'Hi! What is your name? : ', 0
-    input_name db "%s %s %s", 0
+    input_name db '%s', 0
 
     prompt2 db 'How old are you? : ', 0
-    input_age db "%s %s %s", 0
+    input_age db '%s', 0
 
     prompt3 db 'Where do you live? : ', 0
-    input_address db "%s %s %s", 0
+    input_address db '%s', 0
 
     output_msg db 'Nice to meet you, %s, who is %s years old and lives at %s', 0
 
-section.bss
+section.bss ; section.bss is where the uninitialized data is stored. We used userName, userAge, and userAddress.
     userName resb 100
     userAge resd 100
     userAddress resb 100
 
-section.text
+section.text ;  section .text is used for declaring the code or instructions.
     global _main
     extern _printf
     extern _scanf
